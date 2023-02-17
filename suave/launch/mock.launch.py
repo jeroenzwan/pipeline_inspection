8e803c921fd742556533577477e4f3183dd0d851
Jeroen <j.zwanepol@hotmail.com>
2023-02-17 10:23:00 +0100

    added launch files and adjusted setup.py for mock execution

---
+++ suave/launch/mock.launch.py
@@ -0,0 +1,76 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    mission_config = os.path.join(
+        get_package_share_directory('suave_missions'),
+        'config',
+        'mission_config.yaml')
+
+    # water_visibility_node = Node(
+    #     package='suave',
+    #     executable='water_visibility_observer',
+    #     name='water_visibility_observer_node',
+    #     parameters=[mission_config],
+    # )
+
+    # pipeline_detection_wv_node = Node(
+    #     package='suave',
+    #     executable='pipeline_detection_wv',
+    # )
+
+    # thruster_monitor_node = Node(
+    #     package='suave',
+    #     executable='thruster_monitor',
+    #     name='thruster_monitor',
+    #     parameters=[mission_config]
+    # )
+
+    spiral_search_mock_node = Node(
+        package='suave',
+        executable='spiral_search_mock',
+        output='screen'
+    )
+
+    follow_pipeline_mock_node = Node(
+        package='suave',
+        executable='follow_pipeline_mock',
+        output='screen',
+    )
+
+    maintain_motion_mock_node = Node(
+        package='suave',
+        executable='maintain_motion_mock',
+        output='screen',
+    )
+
+    pkg_suave_path = get_package_share_directory('suave')
+
+    system_modes_mock_launch_path = os.path.join(
+        pkg_suave_path,
+        'launch',
+        'system_modes_mock.launch.py')
+
+    system_modes_mock_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(system_modes_mock_launch_path))
+
+    return LaunchDescription([
+        # water_visibility_node,
+        # pipeline_detection_wv_node,
+        # thruster_monitor_node,
+        spiral_search_mock_node,
+        follow_pipeline_mock_node,
+        maintain_motion_mock_node,
+        system_modes_mock_launch,
+    ])
